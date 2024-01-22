33f0da72497c6b7f70cbb0d6ee448de93e2fd665
Pedro Magno <pedromagnopro@gmail.com>
2024-01-22 04:32:03 -0300

    Projeto Converter Idade em dias concluído

---
+++ ConverterIdadeEmDias/ConverterIdadeEmDias/Program.cs
@@ -0,0 +1,15 @@
+﻿using ConverterIdadeEmDias;
+
+Console.WriteLine("Bem-vindo(a) ao conversor de idade em dias!");
+try
+{
+    Console.Write("Informe a sua idade: ");
+    int idade = int.Parse(Console.ReadLine()!);
+    int diasDeVida = Conversor.IdadeEmDias(idade);
+    Console.WriteLine($"Até o momento, você teve {diasDeVida} dias de vida.");
+}
+catch(Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
+
