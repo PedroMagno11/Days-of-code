d760285c4fd5b33e8b421978d40c189c1fd8c782
Pedro Magno <pedromagnopro@gmail.com>
2024-01-25 09:53:11 -0300

    Projeto churrasqueira concluído

---
+++ Espetos de Churrasco/Espetos de Churrasco/Program.cs
@@ -0,0 +1,19 @@
+﻿using Espetos_de_Churrasco;
+
+string[] espetos = new string[]
+{ 
+    "--oooo-ooo--", 
+    "--xx--x--xx--", 
+    "--o---o--oo--", 
+    "--xx--x--ox--", 
+    "--xx--x--ox--"
+};
+
+(int quantEspetosDeCarne, int quantEspetosVegetarianos) = Churrasqueira.QuantidadeDeTiposDeEspeto(espetos);
+
+foreach (var espeto in espetos)
+{
+    Console.WriteLine(espeto);
+}
+Console.Write(" ->");
+Console.WriteLine($"[{quantEspetosVegetarianos} espetos vegetarianos, {quantEspetosDeCarne} espetos de carne]");
---
+++ Espetos de Churrasco/EspetosDeChurrascoTests/ChurrasqueiraUnitTests.cs
@@ -0,0 +1,92 @@
+using Espetos_de_Churrasco;
+
+namespace EspetosDeChurrascoUnitTests
+{
+    public class ChurrasqueiraUnitTests
+    {
+        [Fact]
+        public void Encontrar2EspetosVegetarianosE3EspetosDeCarne()
+        {
+            string[] espetos =
+            {
+                "--oooo-ooo--",
+                "--xx--x--xx--",
+                "--o---o--oo--",
+                "--xx--x--ox--",
+                "--xx--x--ox--"
+            };
+
+            int espetosDeCarne = 3;
+            int espetosVegetarianos = 2;
+
+            (int actualEspetosDeCarne, int actualEspetosVegetarianos) = Churrasqueira.QuantidadeDeTiposDeEspeto(espetos);
+
+            bool resultCarne = (espetosDeCarne == actualEspetosDeCarne);
+            bool resultVegetarianos = (espetosVegetarianos == actualEspetosVegetarianos);
+
+            Assert.True(resultCarne == resultVegetarianos);
+        }
+
+        [Fact]
+        public void Encontrar1EspetosVegetarianosE4EspetosDeCarne()
+        {
+            string[] espetos =
+            {
+                "--oooo-ooo--",
+                "--xx--x--xx--",
+                "--o---o--ox--",
+                "--xx--x--ox--",
+                "--xx--x--ox--"
+            };
+
+            int espetosDeCarne = 4;
+            int espetosVegetarianos = 1;
+
+            (int actualEspetosDeCarne, int actualEspetosVegetarianos) = Churrasqueira.QuantidadeDeTiposDeEspeto(espetos);
+
+            bool resultCarne = (espetosDeCarne == actualEspetosDeCarne);
+            bool resultVegetarianos = (espetosVegetarianos == actualEspetosVegetarianos);
+
+            Assert.True(resultCarne == resultVegetarianos);
+        }
+
+        [Fact]
+        public void Encontrar0EspetosVegetarianosE2EspetosDeCarne()
+        {
+            string[] espetos =
+            {
+                "--oooo-oxo--",
+                "--xx--x--xx--"
+            };
+
+            int espetosDeCarne = 2;
+            int espetosVegetarianos = 0;
+
+            (int actualEspetosDeCarne, int actualEspetosVegetarianos) = Churrasqueira.QuantidadeDeTiposDeEspeto(espetos);
+
+            bool resultCarne = (espetosDeCarne == actualEspetosDeCarne);
+            bool resultVegetarianos = (espetosVegetarianos == actualEspetosVegetarianos);
+
+            Assert.True(resultCarne == resultVegetarianos);
+        }
+
+        [Fact]
+        public void Encontrar0EspetosVegetarianosE0EspetosDeCarne()
+        {
+            string[] espetos =
+            {
+
+            };
+
+            int espetosDeCarne = 0;
+            int espetosVegetarianos = 0;
+
+            (int actualEspetosDeCarne, int actualEspetosVegetarianos) = Churrasqueira.QuantidadeDeTiposDeEspeto(espetos);
+
+            bool resultCarne = (espetosDeCarne == actualEspetosDeCarne);
+            bool resultVegetarianos = (espetosVegetarianos == actualEspetosVegetarianos);
+
+            Assert.True(resultCarne == resultVegetarianos);
+        }
+    }
+}
